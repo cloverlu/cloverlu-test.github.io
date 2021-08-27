89be6c9ee1b071eafc99bf07d24d0c4ec595a728
彭露 <penglu@ti-net.com.cn>
2021-08-27 15:33:18 +0800

    config文件修改

--- vue.config.js
+++ vue.config.js
@@ -2,25 +2,13 @@
 module.exports = {
   productionSourceMap: false,
   // 基本路径
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/cloverlu-test.github.io" : "/",
   // 输出文件目录
   outputDir: "dist",
   devServer: {
     // 设置默认端口
-    port: 8080,
+    port: 8080
     // 设置代理
-    proxy: {
-      "/alm": {
-        // alm开发环境API地址
-        target: "http://20.147.168.82:9001",
-        // 如果要代理 websockets
-        ws: true,
-        // 将主机标头的原点更改为目标URL
-        changeOrigin: true,
-        pathRewrite: {
-          "^/alm": "/"
-        }
-      }
-    }
   }
 };
